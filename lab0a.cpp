069e9b356da383f324f4489fbcfda90994117eef
Julio Montes <juliomontes@Julios-MacBook-Pro.local>
2019-08-21 15:33:49 -0700

    first commit

---
+++ lab0a.cpp
@@ -0,0 +1,12 @@
+// First C++ Program
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "The start of my successful career" << endl;
+    cout << "as a master programmer" << endl;
+    return 0;
+}
---
+++ lab0b.cpp
@@ -0,0 +1,17 @@
+// Printing a triangle
+
+#include <iostream>
+
+using namespace std;
+
+void triangle() {
+    cout << "  *\n ***\n*****\n";
+}
+
+int main()
+{
+    cout << "Wait for it folks..." << endl;
+    triangle();
+    cout << "That's all folks!" << endl;
+    return 0;
+}
