0e118eb8e243aa4093750d3bec5057518aaf882e
Julio Montes <juliomontes@julios-mbp.federated.fds>
2019-08-30 12:11:45 -0700

    first lab, radius problems

--- lab1.cpp
+++ lab1.cpp
@@ -2,33 +2,53 @@
 
 #include <iostream>
 #include <cmath>
+#include <sstream>
+#include <iomanip>
+
 using namespace std;
 
 short distance(short x1, short x2, short y1, short y2);
-short area(double radius);
+double area(double radius);
+double circumference(double radius);
+double volume(double radius);
+double surfaceArea(double radius);
 
-int main()
+    int main()
 {
     // local variables
     short x1, x2, y1, y2, distanceBetweenCoordinates;
-    double radius, pi, areaOfCircle;
+    double radius, pi, areaOfCircle, circumferenceOfCircle, volumeOfSphere, surfaceAreaOfSphere;
 
-    cout << "###########################################################################\n";
-    cout << "###\t\t\t    ANNUAL CONFERENCE    \t\t\t###\n";
-    cout << "###########################################################################\n";
-    cout << "### Name: \t\t\t\t\t\t\t\t###\n";
-    cout << "###        \t\t\t\t\t\t\t\t###\n";
-    cout << "###########################################################################\n";
-    cout << "### Organization: \t\t\t\t\t\t\t###\n";
-    cout << "###        \t\t\t\t\t\t\t\t###\n";
-    cout << "###########################################################################\n";
+    // cout << "###########################################################################\n";
+    // cout << "###\t\t\t    ANNUAL CONFERENCE    \t\t\t###\n";
+    // cout << "###########################################################################\n";
+    // cout << "### Name: \t\t\t\t\t\t\t\t###\n";
+    // cout << "###        \t\t\t\t\t\t\t\t###\n";
+    // cout << "###########################################################################\n";
+    // cout << "### Organization: \t\t\t\t\t\t\t###\n";
+    // cout << "###        \t\t\t\t\t\t\t\t###\n";
+    // cout << "###########################################################################\n";
 
     //calling functions
-    distanceBetweenCoordinates = distance(x1, y1, x2, y2);
-    cout << distanceBetweenCoordinates << endl;
+    // distanceBetweenCoordinates = distance(x1, y1, x2, y2);
+    // cout << distanceBetweenCoordinates << endl;
+
+    radius = 7.5;
+    // cout << "Enter radius: ";
+    // cin >> radius;
 
     areaOfCircle = area(radius);
-    cout << areaOfCircle << endl;
+    cout << "The area of a circle is " << areaOfCircle << endl;
+
+    circumferenceOfCircle = circumference(radius);
+    cout << "The circumference of a circle is " << circumferenceOfCircle << endl;
+
+    volumeOfSphere = volume(radius);
+    cout << "The volume of a sphere is " << volumeOfSphere << endl;
+
+    surfaceAreaOfSphere = surfaceArea(radius);
+    cout << "The surface Area of a sphere is " << setprecision(5) << surfaceAreaOfSphere << endl;
+
     return 0;
 }
 
@@ -51,14 +71,49 @@ short distance(short x1, short x2, short y1, short y2)
     return result;
 }
 
+
+// function to find area
 double area(double radius)
 {
     const double PI = 3.14;
     double result;
 
-    cout << "Enter radius: ";
-    cin >> radius;
+    // cout << "Enter radius: ";
+    // getline(cin,radius);
+    // radius = 7.5;
     result = PI * pow(radius, 2);
+    // cout << "The area of a circle is " << result << endl;
 
+    return result;
+}
+
+// function to find circumference
+double circumference(double radius)
+{
+    const double PI = 3.14;
+    double result;
+
+    // cout << "Enter radius: ";
+    // getline(cin,radius);
+    // radius = 7.5;
+    result = 2.0 * PI * radius;
+    // cout << "The circumference of a circle is " << result << endl;
+
+    return result;
+}
+
+double volume(double radius) {
+    const double PI = 3.14;
+    double result;
+
+    result = ((4.0/3) * PI * pow(radius,3));
+    return result;
+}
+
+double surfaceArea(double radius) {
+    const double PI = 3.14;
+    double result;
+    
+    result = 4.0 * PI * pow((radius),2);
     return result;
 }
