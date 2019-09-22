12a4481db3b58774cb387dbb08c28b2f665101c2
Julio Montes <juliocmontes.a@gmail.com>
2019-09-22 08:46:52 -0700

    finished multiplying by addition

--- lab1_ma.cpp
+++ lab1_ma.cpp
@@ -6,13 +6,18 @@ int main()
 {
     int counter, multiplier, number, result;
 
-    cout << "Enter two numbers: " << endl;
-    cin >> multiplier >> number;
+    cout << "Enter the times you want it multiplied: ";
+    cin >> multiplier;
+    cout << "Enter a number: ";
+    cin >> number;
 
-    for (counter = 0, counter <= multiplier, counter++)
+    result = 0;
+
+    for (counter = 1; counter <= multiplier; counter++)
     {
-        result =
+        result += number;
     }
 
+    cout << "Multiplication of " << multiplier << " & " << number << " is " << result << endl;
+
     return 0;
-}
