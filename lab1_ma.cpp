1b1e3d430b3d35052fa879894b41281034e25f0b
Julio Montes <juliocmontes.a@gmail.com>
2019-09-21 20:12:16 -0700

    directory cleaning

---
+++ lab1_ma.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int counter, multiplier, number, result;
+
+    cout << "Enter two numbers: " << endl;
+    cin >> multiplier >> number;
+
+    for (counter = 0, counter <= multiplier, counter++)
+    {
+        result =
+    }
+
+    return 0;
+}
