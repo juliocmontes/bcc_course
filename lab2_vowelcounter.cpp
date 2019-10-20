5c019a7800b4d85aaa0f62e0a05e1f2b6eadd339
Julio Montes <juliocmontes.a@gmail.com>
2019-10-20 10:53:51 -0700

    finished lab2

--- lab2_vowelcounter.cpp
+++ lab2_vowelcounter.cpp
@@ -3,7 +3,12 @@ Title: Lab 2
 Author: Julio Montes
 Date: 09.23.19
 
-Description: pass the number of rows needed
+Must compile with C11 for the following loop method
+
+const char vowels[]= {'a', 'e', 'i', 'o', 'u', 'y'};
+for (char letter : vowels)
+
+Description: passing a single word string will loop through and count the number of vowels and consonants 
 */
 
 #include <iostream>
@@ -11,15 +16,30 @@ Description: pass the number of rows needed
 #include <sstream>
 #include <iomanip>
 
+
 using namespace std;
 
 int main() {
-    const int size = 6;
-    const char vowels[size]= {'a', 'e', 'i', 'o', 'u', 'y'};
+    // initializing the word variable
+    const string word = "hippopotamus";
+    // array to hold vowels
+    const char vowels[]= {'a', 'e', 'i', 'o', 'u', 'y'};
+    // counter for vowel
+    int vowelCount = 0;
 
-    for (int count = 0; count <= size; count++) {
-        cout << vowels[count] << endl;   
+    // iterates through the string
+    for( int i = 0; i < word.length(); i++){
+        for (char letter : vowels)
+        // iterates through the string and compares each character to one in the array and counts if it's a vowel
+        if (word[i] == letter) {
+            vowelCount++;
+            }
     }
+    // subtracting vowels from length of  string to find vowels
+    int consonantCount = word.length() - vowelCount;
+
+    // displaying the message to user
+    cout << "The word contains " << vowelCount << " vowels and " << consonantCount << " consonants." <<endl; 
 
     return 0;
 }
