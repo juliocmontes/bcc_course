879ad547d4916f83c52de8582f829831cd1826d3
Julio Montes <juliocmontes.a@gmail.com>
2019-10-20 10:59:32 -0700

    cleaned up files

---
+++ Lab 2/lab2_stardiagram.cpp
@@ -0,0 +1,37 @@
+/*
+Title: Lab 2
+Author: Julio Montes
+Date: 09.23.19
+
+Description: returns a graphical view of 2^n with n being the row numbers
+*/
+
+#include <iostream>
+#include <cmath>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+// void starDiagram(int rows, char var);
+
+int main() {
+    // also n in 2^n will print diagram for it
+    int rows = 6;
+    char var = '*';
+
+    int exponent = 0;
+    // loop to print first rows up 2^6
+    for (int i = 0; i <= rows; i++ )
+    {
+        exponent = pow(2, i);
+        cout << string(exponent, var) << endl;
+    }
+    // loops back down
+    for (int i = 0; i < rows; i++ )
+    {
+        exponent/=2;
+        cout << string(exponent, var) << endl;
+    }
+    return 0;
+}
---
+++ Lab 2/lab2_vowelcounter.cpp
@@ -0,0 +1,45 @@
+/*
+Title: Lab 2
+Author: Julio Montes
+Date: 09.23.19
+
+Must compile with C11 for the following loop method
+
+const char vowels[]= {'a', 'e', 'i', 'o', 'u', 'y'};
+for (char letter : vowels)
+
+Description: passing a single word string will loop through and count the number of vowels and consonants 
+*/
+
+#include <iostream>
+#include <cmath>
+#include <sstream>
+#include <iomanip>
+
+
+using namespace std;
+
+int main() {
+    // initializing the word variable
+    const string word = "hippopotamus";
+    // array to hold vowels
+    const char vowels[]= {'a', 'e', 'i', 'o', 'u', 'y'};
+    // counter for vowel
+    int vowelCount = 0;
+
+    // iterates through the string
+    for( int i = 0; i < word.length(); i++){
+        for (char letter : vowels)
+        // iterates through the string and compares each character to one in the array and counts if it's a vowel
+        if (word[i] == letter) {
+            vowelCount++;
+            }
+    }
+    // subtracting vowels from length of  string to find vowels
+    int consonantCount = word.length() - vowelCount;
+
+    // displaying the message to user
+    cout << "The word contains " << vowelCount << " vowels and " << consonantCount << " consonants." <<endl; 
+    
+    return 0;
+}
